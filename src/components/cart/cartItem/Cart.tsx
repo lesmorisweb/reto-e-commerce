299558c7a9a8c4dd943ac6dc5c60036163999069
lesmorisweb <lesmoris0web@gmail.com>
2023-01-05 22:35:02 -0500

    components developments onboarding

--- src/components/cart/cartItem/Cart.tsx
+++ src/components/cart/cartItem/Cart.tsx
@@ -1,23 +1,31 @@
-import { IYourCart } from "./ICar";
+import { useEffect, useState } from "react";
+import { IncreaseDecreaseButton } from "../../global/buttons/increaseDecreaseButton/IncreaseDecraseButton";
+import { Prices } from "../../global/labels/price/Price";
+import { CartImg } from "../cartImage/CartImage";
+import { IYourCart } from "./types";
 
-export function YourCart ({img, lessButton, numberDishes, plusButton, price, title, totalPrice}:IYourCart){
+export function YourCart ({img, numberDishes, price, title, totalPrice, deleteDish, buttonClick}:IYourCart){
+    
+    const [enabledButton, setEnabledButton] = useState <boolean>(true)
+    useEffect (()=>{
+        setEnabledButton(!(numberDishes<=0))
+    }, [numberDishes])
+
     return (
         <div className="yourCarCard">
-            <div className="titleSection">
-                <h1>YourCart</h1>
-            </div>
+            
             <div className="foodImage">
-                <img src={img}/>     
+                <CartImg img={img} deleteDish = {deleteDish}/>     
             </div>
             <div className="infoFood">
                 <h3>{title}</h3>
                 <h4>{price}</h4>
             </div>
             <div className="buySection">
-                <button>{lessButton}</button>
-                <button>{numberDishes}</button>
-                <button>{plusButton}</button>
-                <h2>{totalPrice}</h2>
+                <IncreaseDecreaseButton buttonClick={buttonClick} direction={"remove"} isEnabled = {enabledButton}/>
+                <h5>{numberDishes}</h5>
+                <IncreaseDecreaseButton buttonClick={buttonClick} direction={"add"} isEnabled = {enabledButton}/>
+                <Prices price={totalPrice}/>
             </div>
             
         </div>
--- src/components/cart/cartItem/types.ts
+++ src/components/cart/cartItem/types.ts
@@ -2,8 +2,8 @@ export interface IYourCart{
     img: string,
     title: string,
     price: number,
-    lessButton: boolean,
-    plusButton: boolean,
     numberDishes: number,
     totalPrice: number
+    deleteDish: ()=> void,
+    buttonClick: ()=> void
 }
